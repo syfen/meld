c0ed38d7329d959b74d7f58d25163a66879e17a2
ToneD <t@syfen.com>
2013-09-22 12:12:55 +1000

    Modifiying parsing to support servers spec lists generated by exhibitor auto discovery

--- src/main/java/com/syfen/zookeeper/util/PropertyParser.java
+++ src/main/java/com/syfen/zookeeper/util/PropertyParser.java
@@ -13,11 +13,13 @@ import java.util.Collection;
  */
 public class PropertyParser {
 
-    private static Logger log = LoggerFactory.getLogger(S3PropertiesFile.class);
+    private static Logger log = LoggerFactory.getLogger(PropertyParser.class);
 
-    public static Collection<String> serverSpecList(String serverSpecs) {
+    public static ArrayList<String> serverSpecList(String serverSpecs) {
 
-        Collection<String> list = new ArrayList<String>();
+        log.debug("Server spec parsing: " + serverSpecs);
+
+        ArrayList<String> list = new ArrayList<String>();
 
         if(serverSpecs != null) {
 
@@ -25,45 +27,74 @@ public class PropertyParser {
 
             // parse each item to extract hostnames to build collection
             for (String spec : serverSpecsList) {
-                if(spec.split(":")[0].equals("S")) {
-                    try {
-                        Integer.parseInt(spec.split(":")[1]);
-                        list.add(spec.split(":")[2]);
-                    }
-                    catch (NumberFormatException e) {
-                        log.error("Invalid exhibitor server spec value");
-                    }
+
+// This works for out of the box exhibitor config but not for auto-discovery mode
+//                if(spec.split(":")[0].equals("S")) {
+//                    try {
+//                        Integer.parseInt(spec.split(":")[1]);
+//                        list.add(spec.split(":")[2]);
+//                    }
+//                    catch (NumberFormatException e) {
+//                        log.error("Invalid exhibitor server spec value");
+//                    }
+//                }
+
+                try {
+                    Integer.parseInt(spec.split(":")[0]);
+                    list.add(spec.split(":")[1]);
+                    log.debug("Server spec adding: " + spec.split(":")[1]);
                 }
+                catch (NumberFormatException e) {
+                    log.error("Invalid exhibitor server spec value");
+                }
+
             }
         }
         return list;
     }
 
-    public static String connectionString(String serverSpecs, int clientPort) {
+    public static String connectionString(String serversSpec, int clientPort) {
 
         StringBuilder sb = new StringBuilder();
 
-        if(serverSpecs != null) {
+        if(serversSpec != null) {
 
-            ArrayList<String> serverSpecsList = new ArrayList<String>(Arrays.asList(serverSpecs.split(",")));
+            ArrayList<String> serverSpecsList = new ArrayList<String>(Arrays.asList(serversSpec.split(",")));
 
-            // parse each item to extract the hostnames then add the port and insert commas
-            for (int i=0; i<serverSpecsList.size(); i++) {
+// This works for out of the box exhibitor config but not for auto-discovery mode
+// parse each item to extract the hostnames then add the port and insert commas
+//            for (int i=0; i<serverSpecsList.size(); i++) {
+//
+//                String spec = serverSpecsList.get(i);
+//                if(spec.split(":")[0].equals("S")) {
+//                    try {
+//                        Integer.parseInt(spec.split(":")[1]);
+//                        sb.append(spec.split(":")[2]);
+//                        sb.append(":");
+//                        sb.append(clientPort);
+//                        if (i < serverSpecsList.size()-1) {
+//                            sb.append(",");
+//                        }
+//                    }
+//                    catch (NumberFormatException e) {
+//                        log.error("Invalid exhibitor server spec value or invalid client port value");
+//                    }
+//                }
+//            }
+            for(int i=0; i<serverSpecsList.size(); i++) {
 
                 String spec = serverSpecsList.get(i);
-                if(spec.split(":")[0].equals("S")) {
-                    try {
-                        Integer.parseInt(spec.split(":")[1]);
-                        sb.append(spec.split(":")[2]);
-                        sb.append(":");
-                        sb.append(clientPort);
-                        if (i < serverSpecsList.size()-1) {
-                            sb.append(",");
-                        }
+                try {
+                    Integer.parseInt(spec.split(":")[0]);
+                    sb.append(spec.split(":")[1]);
+                    sb.append(":");
+                    sb.append(clientPort);
+                    if (i < serverSpecsList.size()-1) {
+                        sb.append(",");
                     }
-                    catch (NumberFormatException e) {
-                        log.error("Invalid exhibitor server spec value or invalid client port value");
-                    }
+                }
+                catch (NumberFormatException e) {
+                    log.error("Invalid exhibitor server spec value or invalid client port value");
                 }
             }
         }
