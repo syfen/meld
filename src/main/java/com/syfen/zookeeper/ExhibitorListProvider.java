c2af2c86b5dd6fbb629eb079730cdb645303b48f
ToneD <t@syfen.com>
2013-09-22 12:11:12 +1000

    Adding some logging and strengthening some types

--- src/main/java/com/syfen/zookeeper/ExhibitorListProvider.java
+++ src/main/java/com/syfen/zookeeper/ExhibitorListProvider.java
@@ -1,6 +1,7 @@
 package com.syfen.zookeeper;
 
 import com.netflix.config.ConfigurationManager;
+import com.syfen.zookeeper.exceptions.PropertyNotFoundException;
 import com.syfen.zookeeper.util.PropertyParser;
 import org.apache.commons.configuration.AbstractConfiguration;
 import com.syfen.zookeeper.util.S3PropertiesFile;
@@ -8,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Collection;
 
 /**
  * User: ToneD
@@ -19,9 +19,9 @@ public class ExhibitorListProvider {
     private static final Logger log = LoggerFactory.getLogger(ExhibitorListProvider.class);
     private static final AbstractConfiguration config = ConfigurationManager.getConfigInstance();
 
-    public static Collection<String> getExhibitorList() {
+    public static ArrayList<String> getExhibitorList() {
 
-        Collection<String> list = new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<String>();
 
         // get exhibitor shared properties file
         S3PropertiesFile props = new S3PropertiesFile(
@@ -30,7 +30,13 @@ public class ExhibitorListProvider {
                 config.getString(Constants.ZK_CONFIG_EXHIBITOR_AWS_ACCESS_KEY),
                 config.getString(Constants.ZK_CONFIG_EXHIBITOR_AWS_SECRET_KEY));
 
-        list.addAll(PropertyParser.serverSpecList(props.get(Constants.ZK_CONFIG_EXHIBITOR_SERVER_SPEC)));
+        try {
+            log.debug("Exhibitor server spec: " + props.get(Constants.ZK_CONFIG_EXHIBITOR_SERVER_SPEC));
+            list.addAll(PropertyParser.serverSpecList(props.get(Constants.ZK_CONFIG_EXHIBITOR_SERVER_SPEC)));
+        }
+        catch(PropertyNotFoundException e) {
+            log.error(e.getMessage());
+        }
 
         log.debug("Exhibitor list: " + list.toString());
 
